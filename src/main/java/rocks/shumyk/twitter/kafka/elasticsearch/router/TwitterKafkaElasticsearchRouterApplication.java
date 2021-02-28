ab73cef0877e2bdf03e863e1080227f4abcfa272
Shumyk <dima.shumyk@gmail.com>
2021-02-28 09:23:17 +0300

    initial commit of empty spring boot application

---
+++ src/main/java/rocks/shumyk/twitter/kafka/elasticsearch/router/TwitterKafkaElasticsearchRouterApplication.java
@@ -0,0 +1,13 @@
+package rocks.shumyk.twitter.kafka.elasticsearch.router;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TwitterKafkaElasticsearchRouterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TwitterKafkaElasticsearchRouterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/rocks/shumyk/twitter/kafka/elasticsearch/router/TwitterKafkaElasticsearchRouterApplicationTests.java
@@ -0,0 +1,13 @@
+package rocks.shumyk.twitter.kafka.elasticsearch.router;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TwitterKafkaElasticsearchRouterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
